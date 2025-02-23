5242298cf1f14c1a5adc90d8efe654fa43c6713c
kaluumah <winnerkalu1303@gmail.com>
2025-02-23 17:54:21 +0100

    Style: Styling the nav bar in the HeroSection fle
    I have styled the navabar in the herosection file

--- frontend/src/components/home/HeroSection.jsx
+++ frontend/src/components/home/HeroSection.jsx
@@ -4,13 +4,24 @@ import logo from "./../../assets/logo.svg";
 // src/components/HomePage/HeroSection
 function HeroSection() {
   return (
-    <section className="flex justify-center items-center ">
-      <div>
-        <nav className="py-20">
-          <div className="leftnavlogoside flex items-center">
-            <img src={logo} className="w-13 h-13"/>
+    <section className="flex justify-center items-center">
+      <div className="py-10">
+        <nav className=" flex justify-center items-center shadow-lg  px-15 py-3">
+          <div className="leftnavlogoside flex items-center mr-30 cursor-pointer">
+            <img src={logo} className="w-13 h-13" />
             <span className="text-white text-2xl">CycleCipher</span>
           </div>
+          {/* Navigation Links */}
+        <div className="flex  items-center space-x-20">
+          <a href="#about" className="navlinks text-2xl text-gray-300 hover:text-white">About</a>
+          <a href="#features" className="navlinks text-gray-300 hover:text-white">Features</a>
+          <a href="#integrations" className="navlinks text-gray-300 hover:text-white">Integrations</a>
+          <a href="#pricing" className="navlinks text-gray-300 hover:text-white">Pricing</a>
+          <a href="#blog" className="navlinks text-gray-300 hover:text-white">Blog</a>
+          <button className="text-2xl bg-purple-800 text-white px-4 py-2 rounded-lg hover:bg-purple-700 ml-30 cursor-pointer">
+            Join Waitlist
+          </button>
+        </div>
         </nav>
       </div>
     </section>
