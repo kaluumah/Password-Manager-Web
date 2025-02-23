8233e72af83209597128127b67c48a00e366af74
kaluumah <winnerkalu1303@gmail.com>
2025-02-23 02:18:46 +0100

    Adjusting the navbar layout

--- frontend/src/components/home/HeroSection.jsx
+++ frontend/src/components/home/HeroSection.jsx
@@ -4,23 +4,15 @@ import logo from "./../../assets/logo.svg";
 // src/components/HomePage/HeroSection
 function HeroSection() {
   return (
-    <section className="flex  items-center justify-center text-center py-20 ">
-      <nav className="flex items-center justify-between px-8 py-4">
-        <div className="logoleft flex items-center ">
-          <img src={logo} className="w-10 h-10" />
-          <span>CycleCipher</span>
-        </div>
-        <div>
-          <ul className="hidden md:flex space-x-20 font-medium text-white">
-            <li className="navlinks">About</li>
-            <li className="navlinks">Features</li>
-            <li className="navlinks">Integrations</li>
-            <li className="navlinks">Pricing</li>
-            <li className="navlinks">Blog</li>
-          </ul>
-        </div>
-      </nav>
-      <div></div>
+    <section className="flex justify-center items-center ">
+      <div>
+        <nav className="py-20">
+          <div className="leftnavlogoside flex items-center">
+            <img src={logo} className="w-13 h-13"/>
+            <span className="text-white text-2xl">CycleCipher</span>
+          </div>
+        </nav>
+      </div>
     </section>
   );
 }
