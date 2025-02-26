724604db14498fafa54b5baf7c3e30eff69ce2b2
kaluumah <winnerkalu1303@gmail.com>
2025-02-26 17:06:17 +0100

    Completed Navbar Text and Responsiveness
    I have completed the navbar, the styling and the page responivesness

--- frontend/src/components/home/HeroSection.jsx
+++ frontend/src/components/home/HeroSection.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "./../shared/Navbar"; // Adjust path as needed
 
+
+
 const HeroSection = () => {
   return (
     <section className=" ">
@@ -9,10 +11,14 @@ const HeroSection = () => {
       </div>
 
       <div class="flex justify-center items-center mt-10">
-  <button class="mvpreleasenotif items-center bg-purple-800 text-white px-4 py-2 rounded-lg hover:bg-purple-700">
-  MVP C 1.0 Release Coming Soon
-  </button>
-</div>
+        <button class="mvpreleasenotif items-center bg-purple-800 text-white px-4 py-2 rounded-lg hover:bg-purple-700">
+          MVP C 1.0 Release Coming Soon
+        </button>
+      </div>
+
+      <div className=" herotextdiv flex text-center text-white ">
+        <h1 className=" herotext mt-15 font-semibold inline-block">The Future of <span className="text-yellow-300">Password Security</span> is Here !</h1>
+      </div>
     </section>
   );
 };
