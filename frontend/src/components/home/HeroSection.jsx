2581b4d96695d269f548e4bdb29b81b48b750470
kaluumah <winnerkalu1303@gmail.com>
2025-02-26 18:07:24 +0100

    Added the hero section image
    I have added the hero image beneath the hero section

--- frontend/src/components/home/HeroSection.jsx
+++ frontend/src/components/home/HeroSection.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./../shared/Navbar"; // Adjust path as needed
-
-
+import heroimage from "./../../assets/heroimage.png";
 
 const HeroSection = () => {
   return (
@@ -17,7 +16,15 @@ const HeroSection = () => {
       </div>
 
       <div className=" herotextdiv flex text-center text-white ">
-        <h1 className=" herotext mt-15 font-semibold inline-block">The Future of <span className="text-yellow-300">Password Security</span> is Here !</h1>
+        <h1 className=" herotext mt-15 font-semibold inline-block">
+          The Future of{" "}
+          <span className="text-yellow-300">Password Security</span> is Here !
+        </h1>
+      </div>
+      <div className=" flex items-center justify-center p-4 mt-20 ">
+      <div className="border rounded-lg ">
+      <img src={heroimage} className="w-300 " />
+      </div>
       </div>
     </section>
   );
