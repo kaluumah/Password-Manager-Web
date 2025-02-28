e9762bf0b82136c7a4a1f19ac1a6373fb98bfb15
kaluumah <winnerkalu1303@gmail.com>
2025-02-28 15:54:56 +0100

    Replaced the Style animation
    I replace the flip animation with a hover effect

--- frontend/src/components/home/AboutSection.jsx
+++ frontend/src/components/home/AboutSection.jsx
@@ -62,34 +62,22 @@ const AboutSection = () => {
       <section className="flex justify-center items-center py-16">
         <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-6 px-6">
           {/* What Card */}
-          <div className="aboutcards flip-card p-6 text-center text-white border-2 border-[#fde047] cursor-pointer">
-            <div className="flip-card-inner">
-              <div className="flip-card-front">
-                <h3 className="text-3xl font-bold mb-4 text-[#d4c2f0]">
-                  What?
-                </h3>
-                <p className="text-palepurple  leading-relaxed">
-                  CycleCipher is a revolutionary platform designed to streamline
-                  and secure your password management, offering a seamless way
-                  to generate, store, and autofill credentials across your
-                  devices. CycleCipher is a revolutionary platform designed to
-                  streamline and secure your password management, offering a
-                  seamless way to generate, store, and autofill credentials
-                  across your devices. CycleCipher is a revolutionary platform
-                  designed to streamline and secure your password management,
-                  offering a seamless way to generate, store, and autofill
-                  credentials across your devices. CycleCipher is a
-                  revolutionary platform designed to streamline and secure your
-                  password management, offering a seamless way to generate,
-                  store, and autofill credentials across your devices.
-                </p>
-              </div>
-
-              <div class="flip-card-back">
-                <p class="title">BACK</p>
-                <p>Leave Me</p>
-              </div>
-            </div>
+          <div className="aboutcards p-6 text-center text-white border-2 border-[#fde047] cursor-pointer">
+            <h3 className="text-3xl font-bold mb-4 text-[#d4c2f0]">What?</h3>
+            <p className="text-palepurple  leading-relaxed">
+              CycleCipher is a revolutionary platform designed to streamline and
+              secure your password management, offering a seamless way to
+              generate, store, and autofill credentials across your devices.
+              CycleCipher is a revolutionary platform designed to streamline and
+              secure your password management, offering a seamless way to
+              generate, store, and autofill credentials across your devices.
+              CycleCipher is a revolutionary platform designed to streamline and
+              secure your password management, offering a seamless way to
+              generate, store, and autofill credentials across your devices.
+              CycleCipher is a revolutionary platform designed to streamline and
+              secure your password management, offering a seamless way to
+              generate, store, and autofill credentials across your devices.
+            </p>
           </div>
 
           {/* Why Card */}
