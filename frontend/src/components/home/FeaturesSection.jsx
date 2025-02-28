caea96282c00f1ae1a5d4e088871d671c852e194
kaluumah <winnerkalu1303@gmail.com>
2025-02-28 19:09:00 +0100

    Created the features section entry animation

--- frontend/src/components/home/FeaturesSection.jsx
+++ frontend/src/components/home/FeaturesSection.jsx
@@ -2,35 +2,38 @@ import React from "react";
 
 const FeaturesSection = () => {
   return (
-    <section classNameName=" featurestopanimate">
-   
-        <div className="boxes">
-          <div className="box">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-          <div className="box">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-          <div className="box">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-          <div className="box">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
+    <section classNameName=" ">
+      {/* Top features entry animation */}
+      <div className="boxes">
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
-     
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+
+      <div className="featurestitle flex text-white justify-center items-center">
+        <h6 className="mt-40">Features</h6>
+      </div>
     </section>
   );
 };
