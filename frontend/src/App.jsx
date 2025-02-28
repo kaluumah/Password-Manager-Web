87ec22d4a7a1bfb226d76ae632fa9d75e5ac67a8
kaluumah <winnerkalu1303@gmail.com>
2025-02-28 17:06:19 +0100

    Started the features section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 //import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HeroSection from "./components/home/HeroSection";
 import AboutSection from "./components/home/AboutSection";
+import FeaturesSection from "./components/home/FeaturesSection";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <HeroSection />
       <AboutSection/>
+      <FeaturesSection/>
     </div>
   );
 }
---
+++ frontend/src/components/home/FeaturesSection.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const FeaturesSection = () => {
+  return (
+    <section classNameName=" featurestopanimate">
+   
+        <div className="boxes">
+          <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+     
+    </section>
+  );
+};
+
+export default FeaturesSection;
