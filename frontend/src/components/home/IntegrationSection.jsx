4575ccc52a66c39e39f582a621ca2f747b352e39
kaluumah <winnerkalu1303@gmail.com>
2025-03-05 15:53:33 +0100

    Created the IntegrationSection

---
+++ frontend/src/components/home/IntegrationSection.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const IntegrationSection = () => {
+  return (
+    <section>
+      <div className="loaderIntegration flex justify-center items-center mt-40">
+        <div class="boxIntegartion box-1Integration">
+          <div class="side-left"></div>
+          <div class="side-right"></div>
+          <div class="side-top"></div>
+        </div>
+        <div class="boxIntegartion box-2Integration">
+          <div class="side-left"></div>
+          <div class="side-right"></div>
+          <div class="side-top"></div>
+        </div>
+        <div class="boxIntegartion box-3Integration">
+          <div class="side-left"></div>
+          <div class="side-right"></div>
+          <div class="side-top"></div>
+        </div>
+        <div class="boxIntegartion box-4Integration">
+          <div class="side-left"></div>
+          <div class="side-right"></div>
+          <div class="side-top"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default IntegrationSection;
