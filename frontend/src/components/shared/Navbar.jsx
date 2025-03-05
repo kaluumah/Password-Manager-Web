e9545ff23dacfae390632fdae30f5da6649b4240
kaluumah <winnerkalu1303@gmail.com>
2025-03-05 16:55:44 +0100

    Fixed the nav bar scroll display
    I added a z index to place the nav bar in front

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import logo from "./../../assets/logo.svg";
 
@@ -20,13 +20,14 @@ function HeroSection() {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-    
+
   return (
     <section className="flex justify-center items-center  w-full">
       <div className="py-10 container mx-auto">
-        <nav className={`fixed top-3  flex justify-center items-center shadow-lg  px-15 py-3 transition-all duration-300 
-          ${scrolling ? " backdrop-blur-md shadow-lg" : "bg-transparent"}`} >
-          
+        <nav
+          className={`fixed top-3  flex justify-center items-center shadow-lg  px-15 py-3 transition-all duration-300 
+          ${scrolling ? " backdrop-blur-md shadow-lg" : "bg-transparent"}`}
+        >
           <div className="leftnavlogoside flex items-center mr-40 cursor-pointer">
             <img src={logo} className="w-13 h-13" />
             <span className="text-white text-2xl">CycleCipher</span>
@@ -51,10 +52,6 @@ function HeroSection() {
             <button className="navbarbutton bg-purple-800 text-white px-4 py-2 rounded-lg hover:bg-purple-700 cursor-pointer ">
               Join Waitlist
             </button>
-
-      
-
-            
           </div>
         </nav>
       </div>
